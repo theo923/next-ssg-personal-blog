4c1893b42115f4c38ba849e3847a5f3a5c15c24c
theo923 <theotam912@gmail.com>
2021-06-29 11:46:56 +0800

    added nav bar & basic flow of layout

---
+++ components/Content.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+const Content = ({item}) => {
+    return (
+        <div className='h-screen' >
+            <div className='flex items-center justify-center bg-gradient-to-tr from-blue-100 via-blue-300 to-blue-500' style={{height: 300}}>
+                <div className=''>Image with header</div>
+            </div>
+            <div className='grid grid-cols-3 justify justify-items-center'>
+                <div className=''>5345435</div>
+                <div className=''>{item}</div>
+                <div className=''>5345435</div>
+                
+            </div>
+        </div>
+
+    )
+    
+}
+
+export default Content;
