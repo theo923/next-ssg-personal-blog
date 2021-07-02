ccf27f39082a11f89c81211d1e01e92ba911fc86
theo923 <theotam912@gmail.com>
2021-07-02 22:27:53 +0800

    fixed the order of category component and issues of AutherSideBar

--- pages/category/[category].js
+++ pages/category/[category].js
@@ -12,7 +12,8 @@ const Catergory = ({category, data, slugs}) => {
     return(
         <>
         <Head>
-            <link href="https://fonts.googleapis.com/css2?family=Anaheim&family=Cinzel+Decorative&family=Hind+Siliguri:wght@300&family=Josefin+Sans:wght@200&family=Jost:ital,wght@1,300&family=Julius+Sans+One&family=Proza+Libre&family=Rajdhani&family=Ubuntu:wght@300&display=swap" rel="stylesheet" />
+            <link href="https://fonts.googleapis.com/css2?family=Anaheim&family=Cinzel+Decorative&family=Hind+Siliguri:wght@300&family=Julius+Sans+One&family=Proza+Libre&family=Rajdhani&family=Ubuntu:wght@300&display=swap" rel="stylesheet" />
+            <link href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@1,300&display=swap" rel="stylesheet" />
             <title>{data.title}</title>
         </Head>
           <Layout data={data} item={slugs} category={category}/>
