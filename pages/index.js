04ccf14553fc0b0ae2fcc1f136c5015e7563d5ab
theo923 <theotam912@gmail.com>
2021-07-03 10:21:43 +0800

    convert this app into PWA & fixing layout2

--- pages/index.js
+++ pages/index.js
@@ -21,18 +21,18 @@ const App = ({data, slugs}) => {
 
             <link rel="manifest" href="/manifest.json" />
             <link
-              href="/icons/icon-16x16.png"
+              href="icons/icon-16x16.png"
               rel="icon"
               type="image/png"
               sizes="16x16"
             />
             <link
-              href="/icons/icon-32x32.png"
+              href="icons/icon-32x32.png"
               rel="icon"
               type="image/png"
               sizes="32x32"
             />
-            <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+            <link rel="apple-touch-icon" href="icons/icon-256x256.png"></link>
             <meta name="theme-color" content="#317EFB" />
             <title>Theo's Blog</title>
         </Head>
