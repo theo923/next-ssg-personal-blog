62822dabcda715e7a5f16c2e215a02a0a383523c
theo923 <83841237+theo923@users.noreply.github.com>
2021-09-10 03:52:30 +0800

    fixes icon for all others page

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -7,51 +7,86 @@ import Head from "next/head";
 import Layout from "../../components/Layout";
 
 const Post = ({ data, httpString }) => {
-  const type = "blog";
-  return (
-    <>
-      <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Anaheim&family=Cinzel+Decorative&family=Hind+Siliguri:wght@300&family=Julius+Sans+One&family=Proza+Libre&family=Rajdhani&family=Spartan:wght@400&family=Ubuntu:wght@300&display=swap"
-          rel="stylesheet"
-        />
-        <link href="../styles/prism-material-dark.css" rel="stylesheet" />
-        <meta name="description" content={httpString} />
-        <meta name="keywords" content={data.tags} />
-        <title>{`${data.title} | Theo's Blog`}</title>
-      </Head>
-      <Layout type={type} data={data} httpString={httpString}></Layout>
-    </>
-  );
+    const type = "blog";
+    return (
+        <>
+            <Head>
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Anaheim&family=Cinzel+Decorative&family=Hind+Siliguri:wght@300&family=Julius+Sans+One&family=Proza+Libre&family=Rajdhani&family=Spartan:wght@400&family=Ubuntu:wght@300&display=swap"
+                    rel="stylesheet"
+                />
+
+                <meta charSet="utf-8" />
+                <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+                <meta
+                    name="viewport"
+                    content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+                />
+                <meta
+                    name="description"
+                    content="Blog about Theo's coding journey, lifestyle, learning different language and modules( React, NextJS, ExpressJS and others.....)."
+                />
+                <meta name="keywords" content="Theo theo923 blog React" />
+
+                <link rel="manifest" href="/manifest.json" />
+                <link
+                    href="icons/icon-16x16.png"
+                    rel="icon"
+                    type="image/png"
+                    sizes="16x16"
+                />
+                <link
+                    href="icons/icon-32x32.png"
+                    rel="icon"
+                    type="image/png"
+                    sizes="32x32"
+                />
+                <link
+                    rel="apple-touch-icon"
+                    href="icons/icon-256x256.png"
+                ></link>
+                <meta name="theme-color" content="#317EFB" />
+
+                <link
+                    href="../styles/prism-material-dark.css"
+                    rel="stylesheet"
+                />
+                <meta name="description" content={httpString} />
+                <meta name="keywords" content={data.tags} />
+                <title>{`${data.title} | Theo's Blog`}</title>
+            </Head>
+            <Layout type={type} data={data} httpString={httpString}></Layout>
+        </>
+    );
 };
 
 export const getStaticPaths = async () => {
-  const files = fs.readdirSync("posts").reverse();
-  const paths = files.map((filename) => ({
-    params: {
-      slug: filename.replace(".md", ""),
-    },
-  }));
+    const files = fs.readdirSync("posts").reverse();
+    const paths = files.map((filename) => ({
+        params: {
+            slug: filename.replace(".md", ""),
+        },
+    }));
 
-  return {
-    paths: paths,
-    fallback: false,
-  };
+    return {
+        paths: paths,
+        fallback: false,
+    };
 };
 
 export const getStaticProps = async ({ params: { slug } }) => {
-  const readMarkdownFile = fs
-    .readFileSync(path.join("posts", slug + ".md"), "utf8")
-    .toString();
-  const matterMarkdownFile = matter(readMarkdownFile);
-  const httpString = marked(matterMarkdownFile.content);
+    const readMarkdownFile = fs
+        .readFileSync(path.join("posts", slug + ".md"), "utf8")
+        .toString();
+    const matterMarkdownFile = matter(readMarkdownFile);
+    const httpString = marked(matterMarkdownFile.content);
 
-  return {
-    props: {
-      httpString,
-      data: matterMarkdownFile.data,
-    },
-  };
+    return {
+        props: {
+            httpString,
+            data: matterMarkdownFile.data,
+        },
+    };
 };
 
 export default Post;
--- pages/category/[category].js
+++ pages/category/[category].js
@@ -6,61 +6,95 @@ import Head from "next/head";
 import Layout from "../../components/Layout";
 
 const categories = [
-  "React",
-  "NextJS",
-  "JavaScript",
-  "HTML5",
-  "CSS",
-  "Lifestyle",
-  "Music",
+    "React",
+    "NextJS",
+    "JavaScript",
+    "HTML5",
+    "CSS",
+    "Lifestyle",
+    "Music",
 ];
 
 const Catergory = ({ category, data, slugs }) => {
-  const type = "blog";
-  return (
-    <>
-      <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Anaheim&family=Cinzel+Decorative&family=Hind+Siliguri:wght@300&family=Julius+Sans+One&family=Proza+Libre&family=Rajdhani&family=Spartan:wght@400&family=Ubuntu:wght@300&display=swap"
-          rel="stylesheet"
-        />
-        <meta name="description" content={data.description} />
-        <meta name="keywords" content={data.category} />
-        <title>{`${category} | Theo's Blog`}</title>
-      </Head>
-      <Layout type={type} data={data} item={slugs} category={category} />
-    </>
-  );
+    const type = "blog";
+    return (
+        <>
+            <Head>
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Anaheim&family=Cinzel+Decorative&family=Hind+Siliguri:wght@300&family=Julius+Sans+One&family=Proza+Libre&family=Rajdhani&family=Spartan:wght@400&family=Ubuntu:wght@300&display=swap"
+                    rel="stylesheet"
+                />
+
+                <meta charSet="utf-8" />
+                <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+                <meta
+                    name="viewport"
+                    content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+                />
+                <meta
+                    name="description"
+                    content="Blog about Theo's coding journey, lifestyle, learning different language and modules( React, NextJS, ExpressJS and others.....)."
+                />
+                <meta name="keywords" content="Theo theo923 blog React" />
+
+                <link rel="manifest" href="/manifest.json" />
+                <link
+                    href="icons/icon-16x16.png"
+                    rel="icon"
+                    type="image/png"
+                    sizes="16x16"
+                />
+                <link
+                    href="icons/icon-32x32.png"
+                    rel="icon"
+                    type="image/png"
+                    sizes="32x32"
+                />
+                <link
+                    rel="apple-touch-icon"
+                    href="icons/icon-256x256.png"
+                ></link>
+                <meta name="theme-color" content="#317EFB" />
+
+                <meta name="description" content={data.description} />
+                <meta name="keywords" content={data.category} />
+                <title>{`${category} | Theo's Blog`}</title>
+            </Head>
+            <Layout type={type} data={data} item={slugs} category={category} />
+        </>
+    );
 };
 
 export const getStaticPaths = async () => {
-  const files = categories;
-  const paths = files.map((filename, idx) => ({
-    params: {
-      category: files[idx],
-    },
-  }));
+    const files = categories;
+    const paths = files.map((filename, idx) => ({
+        params: {
+            category: files[idx],
+        },
+    }));
 
-  return {
-    paths: paths,
-    fallback: false,
-  };
+    return {
+        paths: paths,
+        fallback: false,
+    };
 };
 
 export const getStaticProps = async ({ params: { category } }) => {
-  const files = fs.readdirSync("posts", "utf8").reverse();
-  const slugs = files.map((filename) => filename.replace(".md", ""));
-  const readMarkdownFile = files.map((slug) =>
-    fs.readFileSync(path.join("posts", slug)).toString()
-  );
-  const matterMarkdownFile = readMarkdownFile.map((file) => matter(file).data);
-  return {
-    props: {
-      category,
-      slugs,
-      data: matterMarkdownFile,
-    },
-  };
+    const files = fs.readdirSync("posts", "utf8").reverse();
+    const slugs = files.map((filename) => filename.replace(".md", ""));
+    const readMarkdownFile = files.map((slug) =>
+        fs.readFileSync(path.join("posts", slug)).toString()
+    );
+    const matterMarkdownFile = readMarkdownFile.map(
+        (file) => matter(file).data
+    );
+    return {
+        props: {
+            category,
+            slugs,
+            data: matterMarkdownFile,
+        },
+    };
 };
 
 export default Catergory;
