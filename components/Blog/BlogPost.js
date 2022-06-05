1729bca55ed285c587334e01a00138df747de1c0
theo923 <theotam912@gmail.com>
2022-06-05 01:27:42 +0100

    added blogpost for 05/06/2022

--- components/Blog/BlogPost.js
+++ components/Blog/BlogPost.js
@@ -6,6 +6,7 @@ import "prismjs/components/prism-csharp";
 import "prismjs/components/prism-jsx";
 import "prismjs/components/prism-python";
 import "prismjs/components/prism-java";
+import "prismjs/components/prism-scala";
 import SetIntersectionObserver from "../functionality/SetIntersectionObserver";
 import Blind75 from "../External/Blind75";
 import { useRouter } from "next/router";
